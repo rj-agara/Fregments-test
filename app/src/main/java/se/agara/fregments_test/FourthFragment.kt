16b938bc8a394ff5da34b0dfff81362e15fd7fbe
Richard (Hemma) <info@agara.se>
2022-01-25 12:48:57 +0100

    Added simple passing of data, from frag 2 to frag 4.

--- app/src/main/java/se/agara/fregments_test/FourthFragment.kt
+++ app/src/main/java/se/agara/fregments_test/FourthFragment.kt
@@ -1,14 +1,17 @@
 package se.agara.fregments_test
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 
 class FourthFragment : Fragment() {
 
+    var receiveData = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +28,10 @@ class FourthFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        Log.i("myDebug", "Data received " + receiveData.toString())
+
+        view.findViewById<TextView>(R.id.rndNum2LOIDTV).text = receiveData.toString()
 
 
 
--- app/src/main/java/se/agara/fregments_test/SecondFragment.kt
+++ app/src/main/java/se/agara/fregments_test/SecondFragment.kt
@@ -1,18 +1,24 @@
 package se.agara.fregments_test
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 
 
 class SecondFragment : Fragment() {
 
 
+    var randomNumber = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
 
     }
 
@@ -27,10 +33,18 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        randomNumber = (0..100).random() // generated random from 0 to 100
+        view.findViewById<TextView>(R.id.rndNum1LOIDTV).text = randomNumber.toString()
+
         // Knappen som öppnar ThirdFragment, med fungerande Back-knapp
         view.findViewById<Button>(R.id.my2ndFragButtonLOID).setOnClickListener {
+
+            var passDataToFourth = FourthFragment()
+            passDataToFourth.receiveData = randomNumber
+            Log.i("myDebug", "Data passed " + randomNumber.toString())
+
             var fragOpener = requireActivity().supportFragmentManager.beginTransaction()
-            fragOpener.add(R.id.myFragmentContainerViewLOID, FourthFragment())
+            fragOpener.add(R.id.myFragmentContainerViewLOID, passDataToFourth)  // Notera att det inte är fragmentet här, utan variablen
             fragOpener.addToBackStack(null)
             fragOpener.commit()
         }
